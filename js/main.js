9b86bf8b7817d8ebf56a6e8fd7ae0d6096433c45
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2023-02-23 10:01:40 +0100

    Git init...

---
+++ js/main.js
@@ -0,0 +1,35 @@
+console.log("App is running...");
+
+// Opdracht 1
+
+// Opdracht 2
+
+// Opdracht 3
+
+// Opdracht 4
+
+// Opdracht 5
+
+
+
+
+
+const data = `
+    [
+        {
+            "title": "Purina One",
+            "description": "Per zak 800 gr.",
+            "price": 3.49
+        },
+        {
+            "title": "Glorix reinigingsdoekjes",
+            "description": "30 stuks",
+            "price": 0.69
+        },
+        {
+            "title": "Silan wasverzachter",
+            "description": "Per flacon 37 wasbeurten",
+            "price": 2.19
+        }
+    ]
+`;
